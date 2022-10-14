e477734e04d4aa878305ffa06b36ad7b6c4fcbe7
James Fan <9515308james@gmail.com>
2022-10-14 17:36:07 +0800

    answer checking

--- wordle.py
+++ wordle.py
@@ -1,3 +1,13 @@
 if __name__=="__main__":
     user_input = input()
     
+    #specify an answer
+    answer = "apple"
+
+    for i in range(len(user_input)):
+        if user_input[i] == answer[i]:
+            print("A")
+        elif user_input[i] in answer:
+            print("B")
+        else:
+            print("X")        
