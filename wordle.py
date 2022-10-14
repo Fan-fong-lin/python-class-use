7938adcfb029ca069c1a214919b715f97040433c
James Fan <9515308james@gmail.com>
2022-10-14 17:46:59 +0800

    added word saple and answer

--- wordle.py
+++ wordle.py
@@ -1,8 +1,14 @@
+import random
+
 if __name__=="__main__":
     user_input = input()
     
     #specify an answer
-    answer = "apple"
+    f=open("woed.txt","r")
+    dictionary=f.read().splitlines()
+    f.close()
+    answer = random.sample(dictionary,1)[0]
+    print(answer)
 
     for i in range(len(user_input)):
         if user_input[i] == answer[i]:
